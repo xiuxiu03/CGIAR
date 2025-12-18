48e3d6fe6cdff563eae5d10612f8f1484f9133a3
xiuxiu03 <599360191@qq.com>
2025-12-18 11:34:30 +0800

    Update CGIAR-be.py

--- CGIAR-be.py
+++ CGIAR-be.py
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")
 
 import random
 
-def set_seed(seed=123):
+def set_seed(seed=42):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -297,5 +297,6 @@ submission = pd.DataFrame({
 })
 submission.to_csv("submission_time_shifted_transformer_no_prior.csv", index=False)
 print("\n Submission saved to submission_time_shifted_transformer_no_prior.csv")
+
 
 
